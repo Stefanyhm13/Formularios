10dbc228e610dd7b65225124a41d922631d30343
Practicante RRHH <practicante.rrhh@sao6.local>
2024-12-28 11:50:21 -0500

    Actualizado ultimo

--- calculosB.py
+++ calculosB.py
@@ -216,7 +216,14 @@ def clasificar_riesgo(puntaje, clasificacion):
 
 # Función para asignar valores a las respuestas
 def asignar_valor(pregunta_num, respuesta):
-    preguntas_invertidas = { ... }  # Mantén el conjunto de preguntas invertidas como en el código original
+    preguntas_invertidas = { 4, 5, 6, 9, 12, 14, 22, 24, 29, 30,
+                            31, 32, 33, 34, 35, 36, 37, 38, 39,
+                            40, 41, 42, 43, 44, 45, 46, 47, 48,
+                            49, 50, 51, 52, 53, 54, 55, 56, 57,
+                            58, 59, 60, 61, 62, 63, 64, 65, 67,
+                            68, 69, 70, 71, 72, 73, 74, 75, 76,
+                            77, 78, 79, 80, 81, 82, 83, 84, 85,
+                            86, 87, 88, 97.}  
 
     if respuesta == "ANULADA":
         return 0
